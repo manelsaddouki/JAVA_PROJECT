e03dd830ae079a5483baa3c1c58119d0f5ff36aa
Imen Cherif <imen.cherif152@gmail.com>
2023-12-09 21:13:14 +0100

    Update SalesReportGenerator.java

--- src/main/java/com/mycompany/final_project/SalesReportGenerator.java
+++ src/main/java/com/mycompany/final_project/SalesReportGenerator.java
@@ -1,23 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.final_project;
-
-/**
- *
- * @author Manel Saddouki
- */
-
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-
-import java.io.IOException;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
 
 public class SalesReportGenerator {
     private List<Transaction> salesTransactions;
@@ -30,7 +18,7 @@ public class SalesReportGenerator {
     // Method to generate sales reports based on different criteria
     public void generateSalesReport() {
         System.out.println("Sales Report: ");
-         
+
         // Generate overall trends report
         generateTrendsReport();
 
@@ -55,8 +43,17 @@ public class SalesReportGenerator {
         // Display sales trends over time
         displaySalesTrends();
 
-        // Export the report to PDF
-        // exportReportToPDF();
+        // Export the sales report to a text file
+        exportReportToFile("SalesReportOutput.txt");
+
+         // Export text file to PDF
+         SalesReportPDFExporter.exportTextFileToPDF("SalesReportOutput.txt");
+
+         // Export text file to PDF
+         SalesChart salesChart = new SalesChart(salesTransactions); // Assuming myTransactions is accessible
+         salesChart.generateSalesTrendsChartToPDF("SalesTrendsChart.pdf");
+
+        
     }
 
     // Defining the different Methods we called earlier:
@@ -194,6 +191,7 @@ public class SalesReportGenerator {
 
         System.out.println("Total Fundraising Revenue: $" + fundraisingRevenue);
         System.out.println("Total Fundraising Profit: $" + fundraisingProfit);
+        System.out.println("-----------------------------");
     }
 
     // Method to calculate average sales
@@ -226,6 +224,7 @@ public class SalesReportGenerator {
         System.out.println("Peak Sales Periods: ");
         System.out.println("Start Date: " + firstTransaction.getDate());
         System.out.println("End Date: " + lastTransaction.getDate());
+        System.out.println("-----------------------------");
     }
 
     // Method to determine the most popular products
@@ -251,6 +250,7 @@ public class SalesReportGenerator {
             Map.Entry<Integer, Integer> entry = sortedProducts.get(i);
             System.out.println("Product ID: " + entry.getKey() + ", Number of Transactions: " + entry.getValue());
         }
+        System.out.println("-----------------------------");
     }
 
     // Display sales trends over time
@@ -260,41 +260,47 @@ public class SalesReportGenerator {
         identifyPeakSalesPeriods(); // Display peak sales periods
     }
 
-    // Method to export the sales report to PDF
-    public void exportReportToPDF() {
-        try {
-            // Create a new PDF document
-            PDDocument document = new PDDocument();
 
-            // Add a blank page to the document
-            PDPage page = new PDPage();
-            document.addPage(page);
-
-            // Create a content stream for adding text to the page
-            PDPageContentStream contentStream = new PDPageContentStream(document, page);
-
-            // Add the sales report content to the PDF
-            contentStream.beginText();
-            contentStream.newLineAtOffset(100, 700); // Set the starting position
-            contentStream.showText("Sales Report");
-
+    // Method to export the sales report to a text file
+    private void exportReportToFile(String filePath) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
             // Generate various reports
-            generateSalesReport(contentStream);
+            generateSalesReportContent(writer);
 
-            contentStream.endText();
-            contentStream.close();
-
-            // Save the PDF document
-            document.save("SalesReport.pdf");
-
-            // Close the document
-            document.close();
-
-            System.out.println("Sales report exported to PDF successfully.");
+            System.out.println("Sales report exported to file successfully.");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    // Method to calculate total revenue
+    private float calculateTotalRevenue() {
+        float totalRevenue = 0;
+        for (Transaction transaction : salesTransactions) {
+            totalRevenue += transaction.getTotalRevenue();
+        }
+        return totalRevenue;
+    }
+
+    // Method to calculate total profit
+    private float calculateTotalProfit() {
+        float totalProfit = 0;
+        for (Transaction transaction : salesTransactions) {
+            totalProfit += transaction.getTotalProfit();
+        }
+        return totalProfit;
+    }
+
+    // Method to generate sales report content and write to file
+    private void generateSalesReportContent(PrintWriter writer) {
+        writer.println("Total Transactions: " + salesTransactions.size());
+        writer.println();
+
+        // Generate overall trends report
+        writer.println("Overall Trends Report: ");
+        writer.println("Total Revenue: $" + calculateTotalRevenue());
+        writer.println("Total Profit: $" + calculateTotalProfit());
+        writer.println();
+    }
 }
